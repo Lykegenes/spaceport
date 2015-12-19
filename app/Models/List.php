b56184c6b8a7a254a7e7c00d8ed63d1b83ca2ac4
Patrick Samson <patricksamson236@gmail.com>
2015-12-18 23:36:05 -0500

    Added List model

---
+++ app/Models/List.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Spaceport\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'lists';
+}
